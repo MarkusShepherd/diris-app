ad9e01eccb924730fc52d3c905bb95744ab82dd1
Markus Shepherd <markus@idtmessaging.com>
2017-04-28 12:01:42 +0300

    update android plugin version

--- scripts/copy_resource_files.js
+++ scripts/copy_resource_files.js
@@ -12,14 +12,6 @@ module.exports = function (ctx) {
         path = ctx.requireCordovaModule('path'),
         rootdir = ctx.opts.projectRoot;
 
-    fs.mkdir('platforms/android/res/raw/', parseInt('777', 8), function (err) {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log("the directory create success");
-        }
-    });
-
     filestocopy.forEach(function (obj) {
         Object.keys(obj).forEach(function (key) {
             var val = obj[key],
